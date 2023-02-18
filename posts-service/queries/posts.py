2259a2e96357ea698a2ff06cd990864bfb038d69
Gabriel Aranda Gaspar <gabrielaranda202@gmail.com>
2023-02-18 05:25:54 +0000

    Get one post endpoint

--- posts-service/queries/posts.py
+++ posts-service/queries/posts.py
@@ -3,6 +3,8 @@ from typing import Optional, List, Union
 from queries.pool import pool
 
 
+class Error(BaseModel):
+    message: str
 class postIn(BaseModel):
     title: str
     description: str
@@ -46,29 +48,58 @@ class PostRepository:
         except Exception:
             return {"message": "could not get all posts"}
 
+    def get_one(self, post_id: int) -> postOut:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        SELECT id
+                            , title
+                            , description
+                            , picture_url
+                            , user_id
+                            , game_id
+                        FROM post
+                        WHERE id = %s
+                        """,
+                        [post_id],
+                    )
+                    record = result.fetchone()
+                    if record is None:
+                        return {"message": "There is no post with that post_id"}
+                    return self.record_in_to_out(record)
+        except Exception as e:
+            print(e)
+            return {"message": "could not get post"}
+
 
     def create(self, post: postIn) -> postOut:
-        with pool.connection() as conn:
-            with conn.cursor() as db:
-                result = db.execute(
-                    """
-                    INSERT INTO post
-                        (title, description, picture_url, user_id, game_id)
-                    Values
-                        (%s, %s, %s, %s, %s)
-                    RETURNING id;
-                    """,
-                    [
-                        post.title,
-                        post.description,
-                        post.picture_url,
-                        post.user_id,
-                        post.game_id
-                    ]
-                )
-                id = result.fetchone()[0]
-                old_data = post.dict()
-                return postOut(id=id, **old_data)
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        INSERT INTO post
+                            (title, description, picture_url, user_id, game_id)
+                        Values
+                            (%s, %s, %s, %s, %s)
+                        RETURNING id;
+                        """,
+                        [
+                            post.title,
+                            post.description,
+                            post.picture_url,
+                            post.user_id,
+                            post.game_id
+                        ]
+                    )
+                    id = result.fetchone()[0]
+                    old_data = post.dict()
+                    return postOut(id=id, **old_data)
+        except Exception as e:
+            print(e)
+            return "Could not create post"
 
 
     def delete(self,post_id: int) -> bool:
@@ -86,3 +117,17 @@ class PostRepository:
         except Exception as e:
             print(e)
             return {"Could not be deleted"}
+
+    def post_into_out(self, id: int, post: postOut):
+        old_data = post.dict()
+        return postOut(id=id, **old_data)
+
+    def record_in_to_out(self, record):
+        return postOut(
+            id=record[0],
+            title=record[1],
+            description=record[2],
+            picture_url=record[3],
+            user_id=record[4],
+            game_id=record[5],
+            )
--- posts-service/routers/posts.py
+++ posts-service/routers/posts.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
-from typing import List
-from queries.posts import postIn, PostRepository, postOut
+from typing import List, Union
+from queries.posts import postIn, PostRepository, postOut, Error
 
 router = APIRouter()
 @router.post("/post")
@@ -21,3 +21,9 @@ def delete_post(
 def get_all_post(
     repo: PostRepository = Depends()):
     return repo.get_all()
+
+@router.get("/post/{post_id}", response_model=Union[postOut, Error])
+def get_post(
+    post_id: int,
+    repo: PostRepository = Depends()):
+    return repo.get_one(post_id)
