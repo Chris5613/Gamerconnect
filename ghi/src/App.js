9d8cd72aa7520b90d03d01307c149b73c1bbbfd3
Dean Davidson <deandavidson37@gmail.com>
2023-02-23 18:14:07 +0000

    List of posts FE

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,20 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Nav from "./components/Home/Nav";
-import MainPage from "./components/Home/MainPage";
-import "./css/homepage.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from './components/Home/Nav';
+import MainPage from './components/Home/MainPage';
+import PostList from './components/Community/PostList';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        {/* <Route path="/posts" element={<MainPage />} />
+function App(props) {
+	return (
+		<BrowserRouter>
+			<Nav />
+			<Routes>
+				<Route path="/" element={<MainPage />} />
+				<Route path="/posts" element={<PostList />} />
+				{/* // <Route path="/signup" element={<SignUpForm />} /> */}
+				{/* <Route path="/posts" element={<MainPage />} />
         <Route path="/events" element={<MainPage />} />
         <Route path="/settings" element={<MainPage />} /> */}
-      </Routes>
-    </BrowserRouter>
-  );
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- posts-service/main.py
+++ posts-service/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import os
 from routers import posts, user, games
 from auth import authenticator 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        os.environ.get("CORS_HOST", "http://localhost:3000")
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(posts.router)
 app.include_router(user.router)
 app.include_router(games.router)
