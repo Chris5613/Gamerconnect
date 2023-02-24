9d0ca50c59f46b9e32dc609ce1ecfe3b1150b1d8
Franky Jiang <frankyscoding@gmail.com>
2023-02-24 19:56:47 +0000

    Games dropdown

--- ghi/src/components/Community/PostList.js
+++ ghi/src/components/Community/PostList.js
@@ -3,6 +3,7 @@ import "./PostList.css";
 
 function PostList() {
   const [posts, setPosts] = useState([]);
+  const [games, setGames] = useState([])
 
   const fetchData = async () => {
     const url = "http://localhost:8001/post";
@@ -13,14 +14,44 @@ function PostList() {
     }
   };
 
+  const [game, setGame] = useState('')
+  const gamechange = (event) => {
+        const value = event.target.value;
+        setGame(value)
+    }
+
+  const getGames = async () => {
+    const url = "http://localhost:8001/games";
+    const response = await fetch(url);
+    if (response.ok) {
+      const game = await response.json();
+      setGames(game);
+    }
+  };
+
   useEffect(() => {
     fetchData();
+	getGames();
   }, []);
 
   return (
+    <div >
+	<div className="dropdown">
+		<select className="dropdown-2" onChange={gamechange} required id="game" name="game" value={game}>
+                            <option className="dropdown-3" value="">Choose a game</option>
+                            {games.map(gamess => {
+                                return (
+                            <option className="dropdown-3" key={gamess.id} value={gamess.title}>
+                            {gamess.title}
+                            </option>
+                    );
+                    })}
+        </select>
+	</div>
     <div className="post-container">
-      <div className="container1">
-        {posts.map((post) => {
+    <div className="container1">
+    {posts.map((post) => {
+		if (post.game === game)
           return (
             <div className="page1">
               <div className="card-deck1 hvr-grow">
@@ -45,7 +76,33 @@ function PostList() {
               </div>
             </div>
           );
+          else
+            return (
+              <div className="page1">
+              <div className="card-deck1 hvr-grow">
+                <div className="card1" key={post.id}>
+                  <img
+                    className="card-img-top1"
+                    src={post.picture_url}
+                    alt="Card cap"
+                  />
+                  <div className="card-body1">
+                    <h5 className="card-title1">{post.title}</h5>
+                    <p className="card-text1">{post.description}</p>
+                  </div>
+                  <div className="card-footer1">
+                    <small className="text-muted1">{post.game}</small>
+                    <small className="text-muted1">@ {post.username}</small>
+                  </div>
+                  <div>
+                    <button className="detail-button1">Post Detail</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            )
         })}
+      </div>
       </div>
     </div>
   );
