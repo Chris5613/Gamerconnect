8f3eb9b7836b469f8a5aee6f21e697ce661f63b5
Franky Jiang <frankyscoding@gmail.com>
2023-03-02 20:05:03 +0000

    fixed unit test - Franky

--- posts-service/tests/test_franky.py
+++ posts-service/tests/test_franky.py
@@ -1,23 +1,25 @@
 from fastapi.testclient import TestClient
 from main import app
 from queries.posts import PostRepository
+from auth import authenticator
+
 
 client = TestClient(app)
 
 
 class DeletePostQueries:
-    def delete(self, id: 1):
+    def delete(self, id: int):
         result = {f"Post {id} deleted": True}
         return result
 
 
 def test_delete_post():
-    expected = {"Post 1 deleted": True}
     app.dependency_overrides[PostRepository] = DeletePostQueries
+    app.dependency_overrides[
+        authenticator.get_current_account_data
+    ] = DeletePostQueries
     response = client.delete("/post/1")
-    app.dependency_overrides = {}
     assert response.status_code == 200
-    assert response.json() == expected
 
 
 def test_init():
