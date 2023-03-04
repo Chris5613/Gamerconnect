ba37abcf4d5f2ab060ed793297231f5bf8b78e93
Christian Wu <christianwu56@gmail.com>
2023-03-04 02:48:23 +0000

    More fixes

--- posts-service/main.py
+++ posts-service/main.py
@@ -6,13 +6,13 @@ from auth import authenticator
 
 
 app = FastAPI()
-origins = [
-    "http://localhost:3000",
-    os.environ.get("CORS_HOST", None),
-]
+# origins = [
+#     "http://localhost:3000",
+#     os.environ.get("CORS_HOST", None),
+# ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
